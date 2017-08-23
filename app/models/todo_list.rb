8ccacae6fb413ffb70c140353eb28c08eaba406b
Ng Thiam Hock <thiamhock.ng.1510@gmail.com>
2017-08-23 10:55:20 +0800

    Include model validations

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,2 +1,13 @@
 class TodoList < ApplicationRecord
+  validates :title, presence: true
+  validates :archive, inclusion: { in: [ true, false ] }
+
+  before_create :set_archive_to_false
+
+  private
+
+    def set_archive_to_false
+      self.archive = false
+    end
+
 end
--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TodoList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:todo_list) { FactoryGirl.build(:todo_list) }
+
+  context "validations" do
+
+    it "should be valid" do
+      expect(todo_list).to be_valid
+    end
+
+    it "is not valid when title is missing" do
+      todo_list.title = "   "
+      expect(todo_list).to_not be_valid
+    end
+
+  end
 end
